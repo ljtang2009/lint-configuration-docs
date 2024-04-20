9564e7e1130494f3719b7a36c0ebb834775a9fbe
Tang Lijin <ho_ho_gl@hotmail.com>
2024-04-20 18:49:34 +0800

    style: :lipstick: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+import { stylelint } from '@ljtang2009/lint-configuration';
+import _ from 'lodash';
+
+/** @type {import('stylelint').Config} */
+export default {
+  ..._.merge(
+    stylelint.buildIn.default,
+    stylelint.order.default,
+    stylelint.prettier.default
+  ),
+};
